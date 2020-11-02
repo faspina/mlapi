9c01a64fd86eb48d99415c11bf41c90c95658554
faspina <faspina@gmail.com>
2020-11-01 20:31:15 -0600

    Fixed it

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-docker run -d --name="mlapi" \
---privileged="true" \
+docker run -d --name='mlapi' \
+--privileged=true \
 -p 5000:5000/tcp \
--e TZ="America/Chicago" \
--e SHMEM="50%" \
--e PUID="99" \
--e PGID="100" \
--e INSTALL_YOLOV3="1" \
--v "./":"/config":rw \
+-e TZ='America/Chicago' \
+-e SHMEM='50%' \
+-e PUID='99' \
+-e PGID='100' \
+-v ~/mlapi:/config:rw \
 faspina1/mlapi
